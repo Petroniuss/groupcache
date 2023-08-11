9685f8dc1345188e4d510bac234beb932ceb6dcc
Patryk Wojtyczek <pwojtyczek@qualtrics.com>
2023-08-11 15:33:24 +0200

    starting axum server

--- groupcache/src/lib.rs
+++ groupcache/src/lib.rs
@@ -5,10 +5,17 @@ extern crate async_trait;
 use std::error::Error;
 use std::future::Future;
 use std::net::{IpAddr, SocketAddr};
+use std::sync::Arc;
 use serde::{Deserialize, Serialize};
 use anyhow::Result;
 use async_trait::async_trait;
-use axum::Router;
+use axum::extract::{Path, State};
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
+use axum::{Json, Router};
+use axum::routing::get;
+use tracing::log;
+use tracing::log::log;
 
 // let's start with defining a couple of traits.
 pub struct Peer {
@@ -26,6 +33,12 @@ pub struct GetResponse {
     value: String,
 }
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct GetResponseFailure {
+    key: String,
+    error: String,
+}
+
 pub trait Transport {
     fn get_rpc(&self, peer: &Peer, req: &GetRequest) -> Result<GetResponse>;
 }
@@ -36,26 +49,29 @@ trait Retriever {
     async fn retrieve(&self, key: &str) -> Result<String>;
 }
 
-pub struct Groupcache {
-    port: u16,
-    base_url: String,
+pub struct Groupcache {}
+
+pub async fn start_server(
+    groupcache: Arc<Groupcache>,
+) -> Result<()> {
+    let app = Router::new()
+        .route("/get/:key_id", get(get_rpc_handler))
+        .with_state(groupcache);
+
+    axum::Server::bind(&SocketAddr::from(([127, 0, 0, 1], 3000)))
+        .serve(app.into_make_service())
+        .await?;
+
+    Ok(())
 }
 
 impl Groupcache {
-    async fn start_server(&self) -> Result<()> {
-        // todo: make sure this is called only once.
-        let app = Router::new()
-            .with_state(1337);
-
-        axum::Server::bind(&SocketAddr::from(([127, 0, 0, 1], self.port)))
-            .serve(app.into_make_service())
-            .await?;
-
-        Ok(())
+    pub fn new() -> Self {
+        Self {}
     }
 
     fn get(&self, key: &str) -> Result<String> {
-        todo!()
+        Err(anyhow::anyhow!("not implemented"))
     }
     // or set_peers
     fn add_peer(&self, node: &Peer) -> Result<()> {
@@ -65,6 +81,31 @@ impl Groupcache {
     fn remove_peer(&self, node: &Peer) -> Result<()> {
         todo!();
     }
+}
+
+async fn get_rpc_handler(
+    Path(key_id): Path<String>,
+    State(groupcache): State<Arc<Groupcache>>,
+) -> Response {
+    log::info!("get_rpc_handler, {}!", key_id);
+
+    return match groupcache.get(&key_id) {
+        Ok(value) => {
+            let response_body = GetResponse {
+                key: key_id,
+                value,
+            };
+            (StatusCode::OK, Json(response_body)).into_response()
+        }
+        Err(error) => {
+            let response_body = GetResponseFailure {
+                key: key_id,
+                error: error.to_string(),
+            };
+
+            (StatusCode::INTERNAL_SERVER_ERROR, Json(response_body)).into_response()
+        }
+    };
 }
 
 pub struct GroupcacheBuilderImpl {
--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,21 @@
-use axum::{routing::{get, post}, http::StatusCode, response::IntoResponse, Json, Router, ServiceExt};
-use serde::{Deserialize, Serialize};
-use std::net::SocketAddr;
-use axum::handler::{Handler, HandlerWithoutStateExt};
+use std::sync::Arc;
+use anyhow::Context;
+use anyhow::Result;
+use groupcache::start_server;
 
 #[tokio::main]
-async fn main() {
-    // initialize tracing
+async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
-    let app = Router::new()
-        .route("/", get(root))
-        .route("/users", post(create_user))
-        .with_state(1337);
-
-    axum::Server::bind(&SocketAddr::from(([127, 0, 0, 1], 3000)))
-        .serve(app.into_make_service())
+    let groupcache = Arc::new(groupcache::Groupcache::new());
+    start_server(groupcache)
         .await
-        .unwrap();
+        .context("failed to start server")?;
+
+    Ok(())
 }
 
 // basic handler that responds with a static string
 async fn root() -> &'static str {
     "Hello, World!"
-}
-
-async fn create_user(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
-    Json(payload): Json<CreateUser>,
-) -> (StatusCode, Json<User>) {
-    // insert your application logic here
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
-
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
-    (StatusCode::CREATED, Json(user))
-}
-
-// the input to our `create_user` handler
-#[derive(Deserialize)]
-struct CreateUser {
-    username: String,
-}
-
-// the output to our `create_user` handler
-#[derive(Serialize)]
-struct User {
-    id: u64,
-    username: String,
 }
