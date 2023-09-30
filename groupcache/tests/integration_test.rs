b52b894d92efacbbfbfd84bfe853bc7a228aedc4
Patryk Wojtyczek <pwojtyczek@qualtrics.com>
2023-09-30 22:26:10 +0200

    integration test verifying errors when one peer is disconnected

--- groupcache/tests/integration_test.rs
+++ groupcache/tests/integration_test.rs
@@ -3,13 +3,16 @@ use anyhow::Result;
 use async_trait::async_trait;
 use groupcache::{GroupcacheWrapper, Key};
 use pretty_assertions::assert_eq;
+use std::future::{pending, Future};
 use std::net::SocketAddr;
 use tokio::net::TcpListener;
+use tokio::sync::oneshot::{Receiver, Sender};
+use tokio_stream::wrappers::TcpListenerStream;
 use tonic::codegen::tokio_stream;
 use tonic::transport::Server;
 
 #[tokio::test]
-pub async fn test_get_without_any_remote_peers() -> Result<()> {
+pub async fn test_get_local_peer() -> Result<()> {
     let groupcache = {
         let loader = TestCacheLoader {
             instance_id: "1".to_string(),
@@ -33,8 +36,8 @@ pub async fn test_get_without_any_remote_peers() -> Result<()> {
 }
 
 #[tokio::test]
-pub async fn test_get_from_a_remote_peer() -> Result<()> {
-    let groupcache = two_connected_instances().await?;
+pub async fn test_get_successful_from_a_remote_peer() -> Result<()> {
+    let (instance_one, instance_two) = two_connected_instances().await?;
 
     async fn assert_lookup(key: &str, groupcache: GroupcacheWrapper<CachedValue>) -> Result<()> {
         let v = groupcache.get(key).await?;
@@ -43,7 +46,37 @@ pub async fn test_get_from_a_remote_peer() -> Result<()> {
     }
 
     for &key in ["K-b3a", "K-karo", "K-x3d", "K-d42", "W-a1a"].iter() {
-        assert_lookup(key, groupcache.clone()).await?;
+        assert_lookup(key, instance_one.clone()).await?;
+        assert_lookup(key, instance_two.clone()).await?;
+    }
+
+    Ok(())
+}
+
+#[tokio::test]
+pub async fn test_failing_get_after_peer_disconnects() -> Result<()> {
+    async fn assert_lookup(key: &str, groupcache: GroupcacheWrapper<CachedValue>) {
+        let result = groupcache.get(key).await;
+        match result {
+            Ok(v) => {
+                assert_eq!(v.contains(key), true);
+            }
+            Err(e) => {
+                let error_string = e.to_string();
+                assert_eq!(
+                    error_string.contains("error"),
+                    true,
+                    "expected error, got: '{}'",
+                    error_string
+                );
+            }
+        }
+    }
+
+    let groupcache = two_instances_with_one_disconnected().await?;
+
+    for &key in ["K-b3a", "K-karo", "K-x3d", "K-d42", "W-a1a"].iter() {
+        assert_lookup(key, groupcache.clone()).await;
     }
 
     Ok(())
@@ -55,12 +88,42 @@ pub async fn test_get_from_a_remote_peer() -> Result<()> {
 
 // todo: test peer joining
 
-async fn two_connected_instances() -> Result<GroupcacheWrapper<CachedValue>> {
+// todo: test peer returning errors
+
+async fn two_connected_instances() -> Result<(
+    GroupcacheWrapper<CachedValue>,
+    GroupcacheWrapper<CachedValue>,
+)> {
     let (instance_one, addr_one) = spawn_groupcache("1".to_string()).await?;
     let (instance_two, addr_two) = spawn_groupcache("2".to_string()).await?;
 
     instance_one.add_peer(addr_two.into()).await?;
     instance_two.add_peer(addr_one.into()).await?;
+
+    Ok((instance_one, instance_two))
+}
+
+async fn two_instances_with_one_disconnected() -> Result<GroupcacheWrapper<CachedValue>> {
+    let (shutdown_signal, shutdown_recv) = tokio::sync::oneshot::channel::<()>();
+    let (shutdown_done_s, shutdown_done_r) = tokio::sync::oneshot::channel::<()>();
+    async fn shutdown_proxy(shutdown_signal: Receiver<()>, shutdown_done: Sender<()>) {
+        shutdown_signal.await.unwrap();
+        shutdown_done.send(()).unwrap();
+    }
+
+    let (instance_one, addr_one) = spawn_groupcache("1".to_string()).await?;
+    let (instance_two, addr_two) = spawn_groupcache_instance(
+        "2".to_string(),
+        "127.0.0.1:0".to_string(),
+        shutdown_proxy(shutdown_recv, shutdown_done_s),
+    )
+    .await?;
+
+    instance_one.add_peer(addr_two.into()).await?;
+    instance_two.add_peer(addr_one.into()).await?;
+
+    shutdown_signal.send(()).unwrap();
+    shutdown_done_r.await.unwrap();
 
     Ok(instance_one)
 }
@@ -68,7 +131,15 @@ async fn two_connected_instances() -> Result<GroupcacheWrapper<CachedValue>> {
 async fn spawn_groupcache(
     instance_id: String,
 ) -> Result<(GroupcacheWrapper<CachedValue>, SocketAddr)> {
-    let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
+    spawn_groupcache_instance(instance_id, "127.0.0.1:0".to_string(), pending()).await
+}
+
+async fn spawn_groupcache_instance(
+    instance_id: String,
+    addr: String,
+    shutdown_signal: impl Future<Output = ()> + Send + 'static,
+) -> Result<(GroupcacheWrapper<CachedValue>, SocketAddr)> {
+    let listener = TcpListener::bind(addr).await.unwrap();
     let addr = listener.local_addr()?;
     let groupcache = {
         let loader = TestCacheLoader { instance_id };
@@ -80,7 +151,7 @@ async fn spawn_groupcache(
     tokio::spawn(async move {
         Server::builder()
             .add_service(server)
-            .serve_with_incoming(tokio_stream::wrappers::TcpListenerStream::new(listener))
+            .serve_with_incoming_shutdown(TcpListenerStream::new(listener), shutdown_signal)
             .await
             .unwrap();
     });
