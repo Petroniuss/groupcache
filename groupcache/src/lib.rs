867248d36ed14a9126419e610d0ae6707c3ca602
Maciej Kozieja <kozieja.maciej@gmail.com>
2023-12-12 00:05:26 +0100

    explicit export with pub use

--- groupcache/src/lib.rs
+++ groupcache/src/lib.rs
@@ -2,7 +2,7 @@
 
 mod errors;
 mod groupcache;
-pub mod groupcache_builder;
+mod groupcache_builder;
 mod groupcache_inner;
 mod http;
 pub mod metrics;
@@ -10,5 +10,6 @@ mod options;
 mod routing;
 
 pub use groupcache::{Groupcache, GroupcachePeer, ValueBounds, ValueLoader};
+pub use groupcache_builder::GroupcacheBuilder;
 pub use groupcache_inner::GroupcacheInner;
 pub use groupcache_pb::GroupcacheServer;
