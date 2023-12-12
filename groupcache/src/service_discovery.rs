b4b095ba419d5a27dd0f34595604f94dbd29ef3a
Maciej Kozieja <kozieja.maciej@gmail.com>
2023-12-12 21:32:22 +0100

    basic delay configuration

--- groupcache/src/service_discovery.rs
+++ groupcache/src/service_discovery.rs
@@ -1,9 +1,13 @@
-use crate::GroupcachePeer;
-use async_trait::async_trait;
 use std::error::Error;
+use std::time::Duration;
+
+use async_trait::async_trait;
+
+use crate::GroupcachePeer;
 
 #[async_trait]
 pub trait ServiceDiscovery: Send {
     async fn initialize(&mut self) -> Result<(), Box<dyn Error>>;
     async fn instances(&self) -> Result<Vec<GroupcachePeer>, Box<dyn Error>>;
+    fn delay(&self) -> Duration;
 }
