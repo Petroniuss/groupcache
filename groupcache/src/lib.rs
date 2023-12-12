ef7373b685b8b8d6da91f883b33380513636f4d5
Maciej Kozieja <kozieja.maciej@gmail.com>
2023-12-12 13:30:04 +0100

    service discovery setup

--- groupcache/src/lib.rs
+++ groupcache/src/lib.rs
@@ -8,8 +8,10 @@ mod http;
 pub mod metrics;
 mod options;
 mod routing;
+mod service_discovery;
 
 pub use groupcache::{Groupcache, GroupcachePeer, ValueBounds, ValueLoader};
 pub use groupcache_builder::GroupcacheBuilder;
 pub use groupcache_inner::GroupcacheInner;
 pub use groupcache_pb::GroupcacheServer;
+pub use service_discovery::{ServiceDiscovery, ServiceDiscoveryError};
