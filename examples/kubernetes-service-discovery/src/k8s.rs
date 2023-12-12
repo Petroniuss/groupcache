25a70558c69b961acb9b1159f9abea7ca02aeb44
Maciej Kozieja <kozieja.maciej@gmail.com>
2023-12-12 22:11:13 +0100

    basic implementation of peer management

--- examples/kubernetes-service-discovery/src/k8s.rs
+++ examples/kubernetes-service-discovery/src/k8s.rs
@@ -27,14 +27,16 @@ impl Kubernetes {
 
 #[async_trait]
 impl ServiceDiscovery for Kubernetes {
-    async fn initialize(&mut self) -> Result<(), Box<dyn Error>> {
+    async fn initialize(&mut self) -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
         let client = Client::try_default().await?;
         let api: Api<Pod> = Api::default_namespaced(client);
         self.api = Some(api);
         Ok(())
     }
 
-    async fn instances(&self) -> Result<Vec<GroupcachePeer>, Box<dyn Error>> {
+    async fn instances(
+        &self,
+    ) -> Result<Vec<GroupcachePeer>, Box<dyn Error + Send + Sync + 'static>> {
         let api = self.api.as_ref().unwrap();
         let pods_with_label_query = ListParams::default().labels("app=groupcache-powered-backend");
         Ok(api
