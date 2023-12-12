87bcadec19768bee4e9dce096d5af9ef3175ff1d
Maciej Kozieja <kozieja.maciej@gmail.com>
2023-12-12 21:24:58 +0100

    add basic initialization logic

--- examples/kubernetes-service-discovery/src/k8s.rs
+++ examples/kubernetes-service-discovery/src/k8s.rs
@@ -6,13 +6,15 @@ use kube::{Api, Client};
 use std::error::Error;
 use std::net::SocketAddr;
 
-pub struct Kubernetes {}
+pub struct Kubernetes {
+    api: Option<Api<Pod>>,
+}
 
 pub struct KubernetesBuilder {}
 
 impl KubernetesBuilder {
     pub fn build(self) -> Kubernetes {
-        Kubernetes {}
+        Kubernetes { api: None }
     }
 }
 
@@ -24,10 +26,15 @@ impl Kubernetes {
 
 #[async_trait]
 impl ServiceDiscovery for Kubernetes {
-    async fn instances(&self) -> Result<Vec<GroupcachePeer>, Box<dyn Error>> {
+    async fn initialize(&mut self) -> Result<(), Box<dyn Error>> {
         let client = Client::try_default().await?;
         let api: Api<Pod> = Api::default_namespaced(client);
+        self.api = Some(api);
+        Ok(())
+    }
 
+    async fn instances(&self) -> Result<Vec<GroupcachePeer>, Box<dyn Error>> {
+        let api = self.api.as_ref().unwrap();
         let pods_with_label_query = ListParams::default().labels("app=groupcache-powered-backend");
         Ok(api
             .list(&pods_with_label_query)
